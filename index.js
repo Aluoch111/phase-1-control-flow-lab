0fb5326abb57fa11ff68d0ca3a09eb5b9ba9a580
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-08-19 01:43:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n){
+  let greeting;
+  if( n <= 400){
+    greeting = "This one is on me!";
+  } if(n >= 2500){
+    greeting = "No can do.";
+  }
+  else if (n > 2000 ){
+    greeting = "I will gladly take your thirty bucks.";
+  }
+ return greeting ;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(n){
+    let greeting;
+    greeting = (n === "NYC") ? "Ok, sounds good." : "No go.";
+    return greeting ;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(n){
+  let greeting ;
+    switch (n){ 
+      case 'not as generous':
+        greeting = "Thank you.";
+         break ;
+      case 'thanks for everything':
+        greeting = "Bye.";
+         break ;
+    default:
+      return "Thank you so much.";
+  }
+  return greeting ;
 }
